7e8598d20db753d69abe923cd88af409680e8faf
Lonn <lonn.wolf031@gmail.com>
2021-04-07 18:19:20 +0200

    update

--- lib/getlocation.dart
+++ lib/getlocation.dart
@@ -12,8 +12,53 @@ class GetLocationWidget extends StatefulWidget {
 class _GetLocationState extends State<GetLocationWidget> {
   final Location location = Location();
 
+  LocationData _currentPosition;
+  String _address,_dateTime;
+  Location location = Location();
+
+
   LocationData? _location;
   String? _error;
+
+  getLoc() async{
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    _currentPosition = await location.getLocation();
+    _initialcameraposition = LatLng(_currentPosition.latitude,_currentPosition.longitude);
+    location.onLocationChanged.listen((LocationData currentLocation) {
+      print("${currentLocation.longitude} : ${currentLocation.longitude}");
+      setState(() {
+        _currentPosition = currentLocation;
+        _initialcameraposition = LatLng(_currentPosition.latitude,_currentPosition.longitude);
+
+        DateTime now = DateTime.now();
+        _dateTime = DateFormat('EEE d MMM kk:mm:ss ').format(now);
+        _getAddress(_currentPosition.latitude, _currentPosition.longitude)
+            .then((value) {
+          setState(() {
+            _address = "${value.first.addressLine}";
+          });
+        });
+      });
+    });
+  }
 
   Future<void> _getLocation() async {
     setState(() {
